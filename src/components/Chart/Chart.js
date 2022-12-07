f2a775e5f14f9b6f544206e3567953317f54f1cd
PatrykCzartowski <patrykczartowski6@gmail.com>
2022-12-07 23:58:14 +0100

    com3

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -3,10 +3,7 @@ import "./Chart.css";
 
 function Chart(props) {
   const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
-    console.log(dataPointValues);
-    // ^ error in assining new values to array
   const totalMaximum = Math.max(...dataPointValues);
-    console.log(totalMaximum);
   return (
     <div className="Chart">
       {props.dataPoints.map((dataPoint) => (
